1f2986861e3c056b81589ff8e4b23152899041ef
otic <oticfgmatvv@gmail.com>
2025-05-19 13:29:23 -0400

    cambio de lugar de fundacupaz pc view en el manifest

--- fundacupaz_phone/__manifest__.py
+++ fundacupaz_phone/__manifest__.py
@@ -4,11 +4,11 @@
     'description': "inventario de telefonos de fundacupaz",
     'depends': ['base', 'web', 'mail', 'fleet', 'hr', 'stock', 'l10n_ve_dpt-10', 'project', 'contacts', 'event'],
     'data': [
+        'views/fundacupaz_pc_views.xml',
         'security/security.xml',
         'security/ir.model.access.csv',
         'views/comisionado_estadal.xml',
         'views/fundacupaz_views.xml',
-        'views/fundacupaz_pc_views.xml',
         'views/fundacupaz_fleet_views.xml',
         'views/fundacupaz_entes_views.xml',
         'views/fundacupaz_inventario_views.xml',
