a9f5a59e4caa11386df5a5f8a20023bdac729b2a
Zero-27k <kevincordova31@gmail.com>
2025-09-16 15:39:16 -0400

    Descripción de mis cambios

--- fundacupaz_phone/__manifest__.py
+++ fundacupaz_phone/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Fundacupaz Phone',
     'version': '1.0',
     'description': "inventario de telefonos de fundacupaz",
-    'depends': ['base', 'web', 'mail', 'fleet', 'hr', 'stock', 'l10n_ve_dpt-10', 'project', 'contacts', 'event'],
+    'depends': ['base','purchase', 'web', 'mail', 'fleet', 'hr', 'stock', 'l10n_ve_dpt-10', 'project', 'contacts', 'event'],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
@@ -14,7 +14,7 @@
         'views/fundacupaz_views.xml',
         'views/fundacupaz_fleet_views.xml',
         'views/fundacupaz_entes_views.xml',
-        'views/fundacupaz_inventario_views.xml',
+        # 'views/fundacupaz_inventario_views.xml',
         'reportes/fundacupaz_reportes_entrega.xml',
         'reportes/report_verificacion_template.xml',
     ],
